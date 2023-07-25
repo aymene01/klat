c35f36528e6278b42ed3b6ca95fa0865b309f9bf
aymene01 <aymene01@hotmail.fr>
2023-07-25 09:37:32 +0200

    add auth module in api

--- apps/api/src/iam/createIamService.ts
+++ apps/api/src/iam/createIamService.ts
@@ -1,7 +1,14 @@
 import { Options } from './types'
+import * as auth from './auth'
+import { partial } from 'lodash'
 
 export const createIamService = (opts: Options) => {
-  return {}
+  return {
+    generateJwt: partial(auth.generateJwt, opts),
+    verifyJwt: partial(auth.verifyJwt, opts),
+    hashPassword: auth.hashPassword,
+    comparePassword: auth.comparePassword,
+  }
 }
 
 export type IamService = ReturnType<typeof createIamService>
