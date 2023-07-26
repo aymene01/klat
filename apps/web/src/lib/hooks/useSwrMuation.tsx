9fc11c19217889cef30717da3668fa58f36305fb
aymene01 <aymene01@hotmail.fr>
2023-07-26 11:41:19 +0200

    remove return in custum hooks

--- apps/web/src/lib/hooks/useSwrMuation.tsx
+++ apps/web/src/lib/hooks/useSwrMuation.tsx
@@ -4,9 +4,8 @@ import { useMutation, UseMutationOptions } from '@tanstack/react-query'
 export const useSwrMutation = <T, K, V extends string>({
   query,
   ...options
-}: UseMutationOptions<{ [key in V]: T }, Error, K, unknown> & { query: string }) => {
-  return useMutation<{ [key in V]: T }, Error, K>({
+}: UseMutationOptions<{ [key in V]: T }, Error, K, unknown> & { query: string }) =>
+  useMutation<{ [key in V]: T }, Error, K>({
     mutationFn: variables => fetcherQL({ query, variables }),
     ...options,
   })
-}
--- apps/web/src/lib/hooks/useSwrQuery.tsx
+++ apps/web/src/lib/hooks/useSwrQuery.tsx
@@ -4,9 +4,8 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query'
 export const useSwrQuery = <T, V extends string>({
   query,
   ...options
-}: UseQueryOptions<{ [key in V]: T }, Error> & { query: string }) => {
-  return useQuery<{ [key in V]: T }, Error>({
+}: UseQueryOptions<{ [key in V]: T }, Error> & { query: string }) =>
+  useQuery<{ [key in V]: T }, Error>({
     queryFn: variables => fetcherQL({ query, variables }),
     ...options,
   })
-}
