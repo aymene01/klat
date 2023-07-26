f853ccff9891758d89df9d15152df05303f72cec
aymene01 <aymene01@hotmail.fr>
2023-07-26 13:56:23 +0200

    add toaster notification

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@klat/ui/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ToastProvider } from '@/lib/context/toast'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <ToastProvider>
+        <Component {...pageProps} />
+      </ToastProvider>
     </QueryClientProvider>
   )
 }
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,38 +1,44 @@
 import { Button } from '@klat/ui'
-// import { useSwrMutation } from '@/lib/hooks/useSwrMuation'
-// import { useSwrQuery } from '@/lib/hooks/useSwrQuery'
+import { useSwrMutation } from '@/lib/hooks/useSwrMuation'
+import { useSwrQuery } from '@/lib/hooks/useSwrQuery'
 import { AuthenticatedUser, MutationCreateAccountArgs, Hello } from '@klat/graphql'
 import createAccount from '@/lib/queries/createAccount'
 import sayHello from '@/lib/queries/sayHello'
+import { toast } from 'react-hot-toast'
 
 export default function Home() {
-  // const { mutate } = useSwrMutation<AuthenticatedUser, MutationCreateAccountArgs, 'createAccount'>({
-  //   query: createAccount,
-  // })
+  const { mutate } = useSwrMutation<AuthenticatedUser, MutationCreateAccountArgs, 'createAccount'>({
+    query: createAccount,
+    onSuccess: data => {
+      toast.success(`Welcome ${data.createAccount.user.username}`)
+    },
+    onError: error => {
+      toast.error(error.message)
+    },
+  })
 
-  // const { data } = useSwrQuery<Hello, 'sayHello'>({
-  //   query: sayHello,
-  //   queryKey: ['sayHello'],
-  // })
+  const { data } = useSwrQuery<Hello, 'sayHello'>({
+    query: sayHello,
+    queryKey: ['sayHello'],
+  })
 
   return (
     <div className="flex justify-center items-center w-full h-screen flex-col space-y-5">
       <h1 className="text-7xl font-extralight">Hello from KLAT App</h1>
       <Button
         onClick={() =>
-          // mutate({
-          //   username: 'helsllo',
-          //   password: 'j',
-          //   passwordConfirmation: 'j',
-          // })
-          alert('hello')
+          mutate({
+            username: 'helsllssso',
+            password: 'j',
+            passwordConfirmation: 'j',
+          })
         }
         variant="primary"
         size="lg"
       >
         Get stated
       </Button>
-      {/* {data && <p>{data.sayHello.message}</p>} */}
+      {data && <p>{data.sayHello.message}</p>}
     </div>
   )
 }
