3f9d247a30f21f171b2c6bde09787aef7eff33c7
aymene01 <aymene01@hotmail.fr>
2023-07-27 14:22:50 +0200

    add react-hook-form

--- apps/web/src/pages/auth/login.tsx
+++ apps/web/src/pages/auth/login.tsx
@@ -1,7 +1,23 @@
+import { useForm } from 'react-hook-form'
+import { Button } from '@klat/ui'
+
+type LoginForm = {
+  username: string
+  password: string
+}
+
 const Login = () => {
+  const { register, formState, handleSubmit } = useForm<LoginForm>()
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data)
+  }
   return (
     <div className="flex justify-center items-center w-full h-screen flex-col space-y-5">
       <h1 className="text-7xl font-extralight">Login page</h1>
+      <input type="text" {...register('username')} name="username" />
+      <input type="text" {...register('password')} name="password" />
+      <Button onClick={handleSubmit(onSubmit)}>Connect</Button>
     </div>
   )
 }
