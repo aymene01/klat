0505ab24440052c59e2949ac754b4a5442a7ca48
aymene01 <aymene01@hotmail.fr>
2023-07-31 22:43:02 +0200

    add user auth in client

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@klat/ui/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ToastProvider } from '@/lib/context/toast'
+import { UserProvider } from '@/lib/context/user'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
-        <Component {...pageProps} />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
       </ToastProvider>
     </QueryClientProvider>
   )
